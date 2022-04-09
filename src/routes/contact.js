b0c6c918b133b5395968d7f5b7eecbc0fa0a8dd8
Marothi Mahlake <code_artist@outlook.com>
2022-04-09 19:12:20 +0200

    Mbongeni's card uncommented, for now...

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const sendEmail = require("../smtp/mail");
+const express = require('express');
+const sendEmail = require('../smtp/mail');
 
 const contactRouter = express.Router();
 
-contactRouter.get("", (req, res) => {
-  res.render("contact", {
-    title: "Contact",
-    layout: "./layouts/cardless-layout",
+contactRouter.get('', (req, res) => {
+  res.render('contact', {
+    title: 'Contact',
+    layout: './layouts/cardless-layout',
   });
 });
 
-contactRouter.post("", (req, res) => {
+contactRouter.post('', (req, res) => {
   // TODO: Retrieve form field data and store the values into variables.
   const { name, email, phone, subject, message } = req.body;
 
@@ -18,10 +18,10 @@ contactRouter.post("", (req, res) => {
 
   sendEmail(email, subject, messageBody, (err, data) => {
     if (err) {
-      res.status(500).json({ msg: "An internal error occurred." });
+      res.status(500).json({ msg: 'An internal error occurred.' });
     } else {
       res.json({
-        msg: "Your message has successfully been sent to MSDC Inc (Pty) Ltd.",
+        msg: 'Your message has successfully been sent to MSDC Inc (Pty) Ltd.',
       });
     }
   });
