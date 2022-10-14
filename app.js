92d1eae7c56bb6a718308efc74143c59f7d08fda
Marothi Mahlake <code_artist@outlook.com>
2022-10-14 08:55:16 +0200

    Updates for portfolio exhibit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const layouts = require('express-ejs-layouts');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3300;
 
 // Middleware configuration.
 app.use(layouts);
